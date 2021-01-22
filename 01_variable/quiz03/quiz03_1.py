de4f7d8da92ec58a3b446653237e8ef6081cac46
marobiana <69413030+marobiana@users.noreply.github.com>
2021-01-22 19:51:34 +0900

    Update quiz03_1.py

---
+++ 01_variable/quiz03/quiz03_1.py
@@ -0,0 +1,8 @@
+# 1. 몫과 나머지
+# 입력1 : 843
+# 입력2 : 8
+number = int(input("입력1 : "))
+number2 = int(input("입력2 : "))
+quotient = number // number2
+remainder = number % number2
+print("몫 : %d 나머지 : %d" % (quotient, remainder))
